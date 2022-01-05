59917e65d4e3cd2469f5ef69b481cfee12131a46
William Roebuck <16RoebuW@fulford.york.sch.uk>
2022-01-05 09:56:51 +0000

    Added recursive factorial calculator

--- factorialFinder/Program.cs
+++ factorialFinder/Program.cs
@@ -11,7 +11,21 @@ namespace factorialFinder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to factorial finder 2022");
+
+            Console.WriteLine("Please enter a number to be calculated recursively");
+            Console.WriteLine(RecursiveFactorial(int.Parse(Console.ReadLine())));
+
             Console.ReadLine();
+
+        }
+
+        private static int RecursiveFactorial(int n)
+        {
+            if (n == 2)
+            {
+                return 2;
+            }
+            return n * RecursiveFactorial(n-1);
         }
     }
 }
